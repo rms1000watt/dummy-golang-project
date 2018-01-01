9737a9e3b6b4743a32cc666185c11fdb5a256728
Ryan Smith <rms1000watt@gmail.com>
2018-01-01 08:48:25 -0800

    added testing in dockerfile & other cleanups

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
-docker build --build-arg GOLANG_DOCKER_VERSION=${GO_DOCKER_VERSION:-1.9.2-alpine3.7} \
+docker build \
+  --build-arg GO_DOCKER_VERSION=${GO_DOCKER_VERSION:-1.9.2-alpine3.7} \
   --force-rm \
   --no-cache \
   --compress \
